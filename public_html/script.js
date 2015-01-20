76ac8e61da9a3f70285df88ae441a4cc8b19406e
Brandon Obermeit <bladegx4@gmail.com>
2015-01-20 13:42:24 -0800

    catch up

---
+++ public_html/script.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$("document").ready(function(){
+
+    $(".practice").css("background","purple");
+    
+    $("p:first").css("color", "white");
+    
+    $("div p:last-child").css("background-color", "cyan");
+    
+    $('div').bind('mouseover', mouseOverme).bind('mouseout', mouseOutme);
+    
+    $('#superHumans').accordion({header: "h3"});
+  
+  
+
+    
+});
+
+function mouseOverme(){
+    $('h1').html("NO TOUCHY");
+}
+
+function mouseOutme(){
+    $('h1').html('PUNCAKE');
+}
